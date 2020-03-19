e5fd5e7561f897a3a6dd90fc379e0a705b6114fb
pham123 <phamdong88@gmail.com>
2020-03-19 15:07:14 +0700

    update

--- index.php
+++ index.php
@@ -34,12 +34,6 @@ $listfile = array_values($scanned_Sub_directory);
   <body>
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
       <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="index.php">FILES</a>
-      <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
-      <!-- <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
-        </li>
-      </ul> -->
     </nav>
 
     <div class="container-fluid">
@@ -53,6 +47,13 @@ $listfile = array_values($scanned_Sub_directory);
                   Home <span class="sr-only">(current)</span>
                 </a>
               </li>
+
+              <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+              <span>Folder List</span>
+              <a class="d-flex align-items-center text-muted" href="#">
+                <span data-feather="plus-circle"></span>
+              </a>
+            </h6>
               
               <?php
               foreach ($listfolder as $key => $value) {
@@ -71,69 +72,10 @@ $listfile = array_values($scanned_Sub_directory);
               }
               ?>
                 
-              
-              <!-- <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="shopping-cart"></span>
-                  Products
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="users"></span>
-                  Customers
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="bar-chart-2"></span>
-                  Reports
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="layers"></span>
-                  Integrations
-                </a>
-              </li> -->
-            </ul>
-
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-              <span>Saved reports</span>
-              <a class="d-flex align-items-center text-muted" href="#">
-                <span data-feather="plus-circle"></span>
-              </a>
-            </h6>
-            <ul class="nav flex-column mb-2">
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Upload file
-                </a>
-              </li>
-
-            </ul>
-          </div>
+            </div>
         </nav>
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-          <!-- <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-            <h1 class="h2">Dashboard</h1>
-            <div class="btn-toolbar mb-2 mb-md-0">
-              <div class="btn-group mr-2">
-                <button class="btn btn-sm btn-outline-secondary">Share</button>
-                <button class="btn btn-sm btn-outline-secondary">Export</button>
-              </div>
-              <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                <span data-feather="calendar"></span>
-                This week
-              </button>
-            </div>
-          </div> -->
-
-          <!-- <canvas class="my-4" id="myChart" width="900" height="380"></canvas> -->
-
-          <!-- <h2>Section title</h2> -->
           <div class="table-responsive">
             <table class="table table-striped table-sm" id='tailieu'>
               <thead>
@@ -184,7 +126,7 @@ $listfile = array_values($scanned_Sub_directory);
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="dist/js/jquery-3.3.1.js"></script>
-    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <!-- <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-slim.min.js"><\/script>')</script> -->
     <script src="dist/js/popper.min.js"></script>
     <script src="dist/js/bootstrap.min.js"></script>
     <script src="dist/js/jquery.dataTables.min.js"></script>
